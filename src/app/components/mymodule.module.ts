0d351b197a7b9f886500b99ce299e15cc7a78a34
ogarijo <ogarijo@birt.eus>
2024-02-12 12:11:04 +0100

    v0.5 se elimina componente de prueba barra-chart

--- src/app/components/mymodule.module.ts
+++ src/app/components/mymodule.module.ts
@@ -3,14 +3,13 @@ import { CommonModule } from '@angular/common';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
-import { BarraChartComponent } from './barra-chart/barra-chart.component';
 
 
 
 @NgModule({
-  declarations: [ BarChartComponent, LineChartComponent, PieChartComponent, BarraChartComponent ],
+  declarations: [ BarChartComponent, LineChartComponent, PieChartComponent ],
   imports: [ CommonModule ],
-  exports: [ BarChartComponent, LineChartComponent, PieChartComponent, BarraChartComponent ]
+  exports: [ BarChartComponent, LineChartComponent, PieChartComponent ]
 })
 
 export class MymoduleModule { }
